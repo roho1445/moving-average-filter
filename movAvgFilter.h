e82b00b8d60d7c6d5152cbd03f3d6564f640a1b4
roho1445 <rvenkatesh2025@ucla.edu>
2023-06-09 00:20:13 -0700

    Added unit_test.cpp

--- movAvgFilter.h
+++ movAvgFilter.h
@@ -1,15 +1,40 @@
 #ifndef MOVAVGFILTER
 #define MOVAVGFILTER
 
-#include <iostream>
 #include <queue>
 
 template<typename Scalar, size_t N>
 class MovingAverageFilter
 {
 public:
-    MovingAverageFilter(); //Adding constructor to initialize private variable
-    Scalar Step(const Scalar& input);
+    MovingAverageFilter() //Adding constructor to initialize private variable
+    {
+        sum = 0; //Using Constructor to initialize private sum variable to 0
+    }
+
+    Scalar Step(const Scalar& input)
+    {
+        //Add input to a sum variable that keeps track of the sum of elements inputted thus far
+        sum += input;
+
+        //Add input to queue keeping track of all elements
+        vals.push(input);
+
+        //Check if queue contains excess elements
+        if (vals.size() > N)
+        {
+            sum -= vals.front();
+            vals.pop();
+        }
+
+        //If Queue is Empty, average is 0 (check to prevent division by 0)
+        if (vals.empty())
+            return 0;
+
+        //Return average of last N elements inputted
+        return sum / N;
+    }
+
 private:
     std::queue<Scalar> vals; //Queue to keep track of last N elements inputted
     Scalar sum; //Using sum variable to track the sum of all elements added thus far
--- unit_test.cpp
+++ unit_test.cpp
@@ -1,39 +1,6 @@
 #include "movAvgFilter.h"
 #include <iostream>
-#include <queue>
 using namespace std;
-
-template<typename Scalar, size_t N>
-MovingAverageFilter<Scalar, N>::MovingAverageFilter()
-{
-    sum = 0; //Using Constructor to initialize private sum variable to 0
-}
-
-
-template<typename Scalar, size_t N>
-Scalar MovingAverageFilter<Scalar, N>::Step(const Scalar& input)
-{
-    //Add input to a sum variable that keeps track of the sum of elements inputted thus far
-    sum += input;
-
-    //Add input to queue keeping track of all elements
-    vals.push(input);
-
-    //Check if queue contains excess elements
-    if(vals.size() > N)
-    {
-        sum -= vals.front();
-        vals.pop();
-    }
-
-    //If Queue is Empty, average is 0 (check to prevent division by 0)
-    if(vals.empty())
-        return 0;
-
-    //Return average of last N elements inputted
-    return sum / vals.size();
-}
-
 
 int main()
 {
