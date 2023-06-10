9d5e4eb1ce187827b2a735b23ab8ac9bc8785ba4
roho1445 <rvenkatesh2025@ucla.edu>
2023-06-09 17:21:49 -0700

    removed using namespace std

--- unit_test.cpp
+++ unit_test.cpp
@@ -1,33 +1,32 @@
 #include "movAvgFilter.h"
 #include <iostream>
-using namespace std;
 
 int main()
 {
     MovingAverageFilter<double, 2> mov; //Initialize Filter to take average of last 2 elements and return average as double
-    cout << "Enter in Number to be Added to Filter (Enter -1 to quit program): "<<endl;
+    std::cout << "Enter in Number to be Added to Filter (Enter -1 to quit program): "<< std::endl;
     double input;
-    cin >> input;
+    std::cin >> input;
 
-    while (!cin)
+    while (!std::cin)
     {
-        cout << "Please Enter Valid Number (Enter -1 to quit program): "<<endl;
-        cin.clear();
-        cin.ignore();
-        cin >> input;
+        std::cout << "Please Enter Valid Number (Enter -1 to quit program): "<< std::endl;
+        std::cin.clear();
+        std::cin.ignore();
+        std::cin >> input;
     }
 
     while(input != -1)
     {
-        cout<< "The Current Step Average is: " << mov.Step(input) <<endl <<endl;
-        cout << "Enter in Number to be Added to Filter (Enter -1 to quit program): "<<endl;
-        cin >> input;
-        while (!cin)
+        std::cout << "The Current Step Average is: " << mov.Step(input) << std::endl << std::endl;
+        std::cout << "Enter in Number to be Added to Filter (Enter -1 to quit program): "<< std::endl;
+        std::cin >> input;
+        while (!std::cin)
         {
-            cout << "Please Enter Valid Number (Enter -1 to quit program): "<<endl;
-            cin.clear();
-            cin.ignore();
-            cin >> input;
+            std::cout << "Please Enter Valid Number (Enter -1 to quit program): "<< std::endl;
+            std::cin.clear();
+            std::cin.ignore();
+            std::cin >> input;
         }
     }
 }
